700ab7d51297760860a207ac1c231d898eea0033
dellabeneta <m.dellabeneta@gmail.com>
2025-01-09 15:49:54 -0300

    Melhoria no install-kubeconfig.sh.

--- scripts/install-kubeconfig.sh
+++ scripts/install-kubeconfig.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Move o arquivo kubeconfig baixado pelo terraform após o provisionamento, para a pasta ~/.kube/
-mv /home/della/projetos/project-argocd/infra/terraform/kubeconfig* ~/.kube/config
+PROJECT_PATH=$(pwd)
+
+mv "$PROJECT_PATH/infra/terraform/kubeconfig" ~/.kube/config
+
 chmod 600 ~/.kube/config
-echo "Arquivo kubeconfig movido com sucesso!"
+
+echo "Arquivo kubeconfig movido para ~/.kube/config com sucesso."

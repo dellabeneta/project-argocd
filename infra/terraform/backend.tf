d25d0a7c69b647d1b2295584ab3c3aeaac93a867
Michel Dellabeneta <85519685+dellabeneta@users.noreply.github.com>
2025-01-10 17:20:04 -0300

    Delete infra/terraform/backend.tf

--- infra/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraformstates.dellabeneta.tech"
-    key            = "project-argocd"
-    region         = "sa-east-1"
-    encrypt        = true
-    dynamodb_table = "terraformstates-lock"
-  }
-}
